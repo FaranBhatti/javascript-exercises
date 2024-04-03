674b7dad612257997d719f56120ea02686d04396
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-04-02 23:21:42 -0700

    done 03 practice reversing a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) 
+{
+    // create variable to store length of the string
+    let lengthOfString = string.length;
 
+    // create reversedString variable and initialize it to be an empty string
+    let reversedString = '';
+
+    // iterate over all the elements of the string provided and add each of those elements to the reversedString until we hit 0
+    for (let i = lengthOfString; i >= 0; i--)
+    {
+        reversedString += string[i];
+    }
+
+    return (reversedString.slice(9));
 };
 
 // Do not edit below this line
