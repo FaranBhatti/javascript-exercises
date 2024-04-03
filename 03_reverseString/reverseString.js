b94ed5f03e40b18208da5d1efd47605d82c9bf14
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-04-03 08:03:21 -0700

    done 03-reverseString all test cases passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,18 +1,6 @@
 const reverseString = function(string) 
 {
-    // create variable to store length of the string
-    let lengthOfString = string.length;
-
-    // create reversedString variable and initialize it to be an empty string
-    let reversedString = '';
-
-    // iterate over all the elements of the string provided and add each of those elements to the reversedString until we hit 0
-    for (let i = lengthOfString; i >= 0; i--)
-    {
-        reversedString += string[i];
-    }
-
-    return (reversedString.slice(9));
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
