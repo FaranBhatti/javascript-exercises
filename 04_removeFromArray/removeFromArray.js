fa06d41325d274e1c1cee8b04caf1df9998516c4
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-04-03 08:03:52 -0700

    finished passing first test case, need to handle multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove) 
+{
+    const lengthOfArray = array.length;
+    let updatedArray = [];
 
+    for (let element in array)
+    {
+        if (array[element] !== elementToRemove)
+        {
+            updatedArray.push(array[element]);
+        }
+    }
+
+    return updatedArray;
 };
 
 // Do not edit below this line
