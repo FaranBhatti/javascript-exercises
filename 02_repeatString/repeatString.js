23866ec1e9096dd47c13496644a860bb4f95156f
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-04-02 22:58:01 -0700

    Done 01, working on 02 - working on implementing last 2 test cases 5/7 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, repeatAmount) {
+    
+    if (repeatAmount < 0)
+    {
+        return 'ERROR';
+    }
+    
+    let finalString = "";
+    
+    for (let i = 0; i < repeatAmount; i++)
+    {
+        finalString += stringToRepeat;
+    }
+    
+    return finalString;
 };
 
 // Do not edit below this line
