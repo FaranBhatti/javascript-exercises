6aca68574fa7493b12a20aee7e5d38b32482f8c1
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-04-04 09:50:32 -0700

    done sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    // create a variable to hold the sum and initialize it to 0
+    let sum = 0;
+
+    if((typeof num1 !== "number") || (typeof num2 !== "number")) 
+    {
+        return 'ERROR';
+    } // End case 1: check if it's a number
+    else if ((num1 < 0) || (num2 < 0)) 
+    {
+        return 'ERROR';
+    }// End case 2: check if it's negative or not
+    else if (num1 > num2)
+    {
+        let holdNum1 = num1;
+        num1 = num2;
+        num2 = holdNum1;
+    } // End case 3: check if num2 is bigger than num1
+    
+    
+    // iterate over that array and add it to the sum
+    for (let i = num1; i <= num2; i++)
+    {
+        sum += i;
+        //arrayOfNums.push(num1 + 1);
+    }
+
+    return sum;
 };
+
+sumAll(10, "90");
 
 // Do not edit below this line
 module.exports = sumAll;
