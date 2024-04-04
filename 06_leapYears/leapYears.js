75433a4b15ff6ad3f1cd69751c75eaee8ec0225c
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-04-04 10:12:09 -0700

    06 leap year done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleBy4 = ((year % 4) == 0);
+    let divisibleBy100 = ((year % 100) == 0);
+    let divisibleBy400 = ((year % 400) == 0);
 
+    if(divisibleBy100 && divisibleBy400) return true;
+    else if(divisibleBy4 && !divisibleBy100) return true;
+    else return false;
 };
+
+//leapYears(1997);
 
 // Do not edit below this line
 module.exports = leapYears;
