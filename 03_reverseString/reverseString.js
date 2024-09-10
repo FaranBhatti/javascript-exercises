ca8cadc0d4172fbe3fc2c9834c5b822947473637
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-10 16:43:50 -0700

    updated solution to reverse string approprioately

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const arrayOfStringChars = string.split("");
+    let reversedString = "";
+
+    for (let i = 1; i <= arrayOfStringChars.length; i++) {
+        reversedString += arrayOfStringChars[arrayOfStringChars.length - i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
