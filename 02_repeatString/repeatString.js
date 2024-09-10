18c6aaea8ef1ccd75344c76342d987f9162a0ff0
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-10 13:14:34 -0700

    updated the repeatString exercise and passed all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = "";
 
+    // if num is below 0 return 'ERROR'
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
