1a7614307f138dbc6d45f2e464dbbc60613d155a
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-10 16:57:34 -0700

    removing single element successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove) {
+
+    const index = array.indexOf(elementToRemove);
+
+    if (index > -1) {
+        
+        array.splice(index, 1);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
