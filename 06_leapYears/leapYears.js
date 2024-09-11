3c64f069fa467d1db30d885704b90b5707e34c48
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-10 17:30:10 -0700

    updated base template

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    // leap years are years divisible by four
+    // however if they're divisible by 100 they are not leap years
+    // unless they're divisible by 400
+    if (year && 4) {
+        if (year && 100) {
 
+            if (year && 400) {
+                
+            }
+        }
+    }
 };
 
 // Do not edit below this line
