1a1b56de774e850d0747bde18366648d8d6a75fd
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-10 17:16:39 -0700

    updated with passing all test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    let sum = 0;
 
+    if (num1 > num2) {
+        sum = num2;
+        num2 = num1;
+        num1 = sum;
+        sum = 0;
+    }
+
+    if ((num1 || num2) < -1) {
+        return 'ERROR'
+    }
+
+    if (!(num1 === Math.floor(num1))) {
+        return 'ERROR'
+    }
+
+    if (!(num2 === Math.floor(num2))) {
+        return 'ERROR'
+    }
+    
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
