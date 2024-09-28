fe86513164da3e433741bc42de8202de294690e3
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-28 14:04:13 -0700

    done calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, nextNum) => {
+    total += nextNum;
+    return total;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, nextNum) => {
+    total *= nextNum;
+    return total; 
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+
+  let result = num;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  
+  return result;
 };
 
 // Do not edit below this line
