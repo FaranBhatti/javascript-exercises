667b073b49a0351ca323a9baa4e5d6dc822690c9
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-10-04 21:21:49 -0700

    updated passes all test cases

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanedString = string.replace(/[\s\'\"\&\n\t\\\r\b\f\v!.,]/gi, '');
 
-};
+    let reversedString = "";
+
+    for(let i = cleanedString.length - 1; i >= 0; i--) {
+        reversedString += cleanedString[i];
+    }
+
+    if(reversedString.toLowerCase() === cleanedString.toLowerCase()) return true;
+
+    return false;
+};  
 
 // Do not edit below this line
 module.exports = palindromes;
