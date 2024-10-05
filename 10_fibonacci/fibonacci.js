be0f9125aa01aad0072de174cbda9227f64653da
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-10-04 21:36:59 -0700

    solved fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS"
 
+    let firstNum = 0;
+    let secondNum = 1;
+
+    if(num == 0) return firstNum;
+    if(num == 1) return secondNum;
+
+    let newNum = 0;
+
+    for(let i = 2; i <= num; i++) {
+        newNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = newNum;
+    }
+
+    return newNum;
 };
 
 // Do not edit below this line
